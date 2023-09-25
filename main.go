1cd0e4c0b8a96133f6d74391bde50347185dadd0
Jordan Sheldon <jordanhsheldon@gmail.com>
2023-09-25 18:16:25 -0400

    added initial files

--- main.go
+++ main.go
@@ -1,24 +1,17 @@
 package main
 
-import (
-	"io/ioutil"
-	"log"
-	"net/http"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	//We Read the response body on the line below.
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	//Convert the body to type string
-	sb := string(body)
-	log.Printf(sb)
+	r := gin.Default()
+	r.POST("/user_data", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"IsDisabled": "false",
+			"username":   "nadroj",
+			"password":   "password",
+			"email":      "nadroj@gmail.com",
+			"settingsid": "1",
+		})
+	})
+	r.Run()
 }
